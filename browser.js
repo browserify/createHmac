d45284c2d26cbadefc577872da461a081fb6147a
Steve Thomas <Sc00bz@users.noreply.github.com>
2018-10-12 07:57:41 -0500

    Allow for fast cached HMACs
    Cloning an `Hmac` object just after creation will allow for cached HMACs. Also if used in PBKDF2 it gets a 2x speed increase and avoids the long password DoS.

--- browser.js
+++ browser.js
@@ -14,8 +14,6 @@ function Hmac (alg, key) {
 
   var blocksize = (alg === 'sha512' || alg === 'sha384') ? 128 : 64
 
-  this._alg = alg
-  this._key = key
   if (key.length > blocksize) {
     var hash = createHash(alg)
     key = hash.update(key).digest()
@@ -24,15 +22,18 @@ function Hmac (alg, key) {
     key = Buffer.concat([key, ZEROS], blocksize)
   }
 
-  var ipad = this._ipad = Buffer.allocUnsafe(blocksize)
-  var opad = this._opad = Buffer.allocUnsafe(blocksize)
+  var ipad = Buffer.allocUnsafe(blocksize)
+  var opad = Buffer.allocUnsafe(blocksize)
 
   for (var i = 0; i < blocksize; i++) {
     ipad[i] = key[i] ^ 0x36
     opad[i] = key[i] ^ 0x5C
   }
-  this._hash = createHash(alg)
-  this._hash.update(ipad)
+
+  this._ihash = createHash(alg)
+  this._ohash = createHash(alg)
+  this._ihash.update(ipad)
+  this._ohash.update(opad)
 }
 
 inherits(Hmac, Base)
@@ -42,9 +43,7 @@ Hmac.prototype._update = function (data) {
 }
 
 Hmac.prototype._final = function () {
-  var h = this._hash.digest()
-  var hash = createHash(this._alg)
-  return hash.update(this._opad).update(h).digest()
+  return this._ohash.update(this._ihash.digest()).digest()
 }
 
 module.exports = function createHmac (alg, key) {
